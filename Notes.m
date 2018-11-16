d3de3255bd9af9b7cca5f3206dd03d4256de7052
Tobias Jacobson <tobi.jacobson@gmail.com>
2018-11-16 12:43:17 -0700

    deleting old files

--- Notes.m
+++
@@ -1,5 +0,0 @@
-% - varargin allows for arbitrary amount of input vectors
-% - notation in our case is capitals for vectors/matrices
-% isstruct determines whether input is structure array(collecting similar
-% data types in a field
-% - opts is defined vector? 
