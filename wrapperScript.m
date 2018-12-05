6569e4b165752ddc62ce8a1d0d8140378e83db73
zgrey <zgrey334@gmail.com>
2018-12-05 13:04:29 -0700

    simple changes to wrapper

--- wrapperScript.m
+++ wrapperScript.m
@@ -1,10 +1,8 @@
-clear all;
-close all;
-clc;
-%function [F] = wrapperScript()
+clearvars; close all; clc;
 [X1,X2] = meshgrid(linspace(-1,1,10),linspace(-1,1,10));
 X = [reshape(X1,10^2,1),reshape(X2,10^2,1)];
-n = input("Enter the digit of the function you want to run");
+n = 7;
+
 if n == 1
     F = func1(X);
     scatter(X(:,1), X(:,2), 30, 'filled', 'cdata', F);
@@ -18,7 +16,7 @@ elseif n == 2
     contour(X1, X2, reshape(F, 10, 10), 30);
     colorbar;
 elseif n == 3
-    a = input("Enter a numerical digit for acceleration phi");
+    a = 1;
     F = func3(X,a);
     scatter(X(:,1), X(:,2), 30, 'filled', 'cdata', F);
     figure;
@@ -31,27 +29,27 @@ elseif n == 4
     contour(X1, X2, reshape(F, 10, 10), 30);
     colorbar;
 elseif n == 5
-    a = input("Enter a number for coefficient #1");
-    b = input("Enter a number for coefficient #2");
-    t = input("Enter a number for Tao");
+    a = 1;
+    b = 2;
+    t = 1;
     F = func5(X, a, b, t);
     scatter(X(:,1), X(:,2), 30, 'filled', 'cdata', F);
     figure;
     contour(X1, X2, reshape(F, 10, 10), 30);
     colorbar;
 elseif n == 6
-    s = input("Enter a value for sigma");
-    a = input("Enter a number for coefficient #1");
-    b = input("Enter a number for coefficient #2");
+    s = 0.1;
+    a = 2;
+    b = 3;
     F = func6(X, s, a, b);
     scatter(X(:,1), X(:,2), 30, 'filled', 'cdata', F);
     figure;
     contour(X1, X2, reshape(F, 10, 10), 30);
     colorbar;
 elseif n == 7
-    s = input("Enter a value for sigma");
-    a = input("Enter a number for coefficient #1");
-    b = input("Enter a number for coefficient #2");
+    s = 1;
+    a = 1;
+    b = 1;
     F = func7(X, s, a, b); 
     scatter(X(:,1), X(:,2), 30, 'filled', 'cdata', F);
     figure;
